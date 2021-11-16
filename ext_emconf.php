bab5486ee08de9e4a88110016c1e63f891e6c0f6
Oliver Thiele <mail@oliver-thiele.de>
2021-11-16 22:06:48 +0100

    [TASK] Release v1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'CE menu cards',
+    'title' => 'CE Menu Cards',
     'description' => 'This content element can be used to output the subpages as Bootstrap Card elements.',
     'category' => 'frontend',
     'author' => 'Oliver Thiele',
     'author_email' => 'mail@oliver-thiele.de',
     'state' => 'stable',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.99.99',
